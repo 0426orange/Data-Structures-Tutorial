7c427f4268fb8c50df6670952f9b64a31591ac85
0426orange <114186672+0426orange@users.noreply.github.com>
2024-12-16 19:46:13 -0700

    Update Solution2.cs

--- Solution2.cs
+++ Solution2.cs
@@ -1,1 +1,21 @@
 
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array1 = { 1, 2, 3, 4, 5 };
+        int[] array2 = { 3, 4, 5, 6, 7 };
+
+        HashSet<int> set1 = new HashSet<int>(array1);
+        HashSet<int> set2 = new HashSet<int>(array2);
+
+        set1.IntersectWith(set2);
+
+        Console.WriteLine("Common Elements:");
+        foreach (var num in set1)
+            Console.WriteLine(num);
+    }
+}
